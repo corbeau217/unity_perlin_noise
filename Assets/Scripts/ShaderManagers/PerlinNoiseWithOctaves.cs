c50668e5a51638773d0029052159508a957b8c35
Corbeau <aurora.griffith@students.mq.edu.au>
2025-09-28 12:40:50 +1000

    refactoring/adding octave code to scene
    copied the wrong shader manager code to use as a base
    also cleaned up the vector creation and cell counts code
    
    seems there's an issue with the way we believed the array loading to gpu was happening so will need to research how to do that if we want to have modifyable octave counts
    otherwise we will need to manually make each of our octave specific variables

--- Assets/Scripts/ShaderManagers/PerlinNoiseWithOctaves.cs
+++ Assets/Scripts/ShaderManagers/PerlinNoiseWithOctaves.cs
@@ -6,21 +6,44 @@ using UnityEngine.UI;
 public class PerlinNoiseWithOctaves : ShaderManager
 {
     [Space(20)]
+    public ShaderManager inputNoiseManager;
     public RawImage previewImage;
     [Space(10)]
     public Vector2Int gridCellCount = Vector2Int.one * 5;
+    [Space(10)]
+    public RenderTexture inputNoiseTexture;
+    public Vector2Int inputDimensions;
 
+    public override void SettingOverrides(){
+        safeToPerform = false;
+    }
     public override void AssignComputeBuffers(int kernelIndex){
         computeShader.SetTexture(kernelIndex, ouputName, outputTexture);
-
         computeShader.SetInt("textureWidth", outputDimensions.x);
         computeShader.SetInt("textureHeight", outputDimensions.y);
+        
+        computeShader.SetTexture(kernelIndex, "InputNoiseImage", inputNoiseTexture);
+        computeShader.SetInt("noiseTextureWidth", inputDimensions.x);
+        computeShader.SetInt("noiseTextureHeight", inputDimensions.y);
 
-        computeShader.SetInt("cellCountX", gridCellCount.x);
-        computeShader.SetInt("cellCountY", gridCellCount.y);
+        float[] cellCountsArray = new float[2];
+        cellCountsArray[0] = gridCellCount.x;
+        cellCountsArray[1] = gridCellCount.y;
+        // TODO : look in to c#/unity structured arrays for being able to create float arrays in hlsl
+        computeShader.SetFloats("cellCounts", cellCountsArray);
     }
     public override void UpdatePreview(){
         previewImage.texture = this.outputTexture;
+    }
+    public override void TestReadyToPerform(){
+        if ( (inputNoiseManager!=null) && (inputNoiseManager.HasPerformedCompute()) ){
+            // grab the things
+            inputNoiseTexture = inputNoiseManager.GetResultTexture();
+            inputDimensions = inputNoiseManager.GetOutputDimensions();
+            // mark as safe
+            safeToPerform = true;
+        }
+        
     }
 
 }
