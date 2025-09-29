3d94fc92c7864d02a543218e19abe6cd8be02452
Corbeau <aurora.griffith@students.mq.edu.au>
2025-09-29 11:29:30 +1000

    bare bones octaved noise

--- Assets/Scripts/ShaderManagers/PerlinNoiseWithOctaves.cs
+++ Assets/Scripts/ShaderManagers/PerlinNoiseWithOctaves.cs
@@ -19,14 +19,22 @@ public class PerlinNoiseWithOctaves : ShaderManager
     public Vector2Int gridCellNoiseOrigin1 = Vector2Int.zero;
     public Vector2Int gridCellCount1 = Vector2Int.one * 4;
     public Matrix4x4 gridCellUVMatrix1 = Matrix4x4.identity;
+    public float octaveContribution1 = 1.0f;
     [Space(15)]
     public Vector2Int gridCellNoiseOrigin2 = Vector2Int.zero;
     public Vector2Int gridCellCount2 = Vector2Int.one * 8;
     public Matrix4x4 gridCellUVMatrix2 = Matrix4x4.identity;
+    public float octaveContribution2 = 0.5f;
     [Space(15)]
     public Vector2Int gridCellNoiseOrigin3 = Vector2Int.zero;
     public Vector2Int gridCellCount3 = Vector2Int.one * 16;
     public Matrix4x4 gridCellUVMatrix3 = Matrix4x4.identity;
+    public float octaveContribution3 = 0.25f;
+    [Space(15)]
+    public Vector2Int gridCellNoiseOrigin4 = Vector2Int.zero;
+    public Vector2Int gridCellCount4 = Vector2Int.one * 16;
+    public Matrix4x4 gridCellUVMatrix4 = Matrix4x4.identity;
+    public float octaveContribution4 = 0.125f;
 
     public override void SettingOverrides(){
         safeToPerform = false;
@@ -41,9 +49,28 @@ public class PerlinNoiseWithOctaves : ShaderManager
         computeShader.SetInt("noiseTextureHeight", inputDimensions.y);
 
         UpdateUVMatrices();
+
         float[] cellCountsArray1 = gridCellCount1.ToFloatArray();
-        computeShader.SetFloats("cellCounts1", cellCountsArray1);
+        float[] cellCountsArray2 = gridCellCount2.ToFloatArray();
+        float[] cellCountsArray3 = gridCellCount3.ToFloatArray();
+        float[] cellCountsArray4 = gridCellCount4.ToFloatArray();
+        
         computeShader.SetMatrix("octaveUVMatrix1", gridCellUVMatrix1);
+        computeShader.SetMatrix("octaveUVMatrix2", gridCellUVMatrix2);
+        computeShader.SetMatrix("octaveUVMatrix3", gridCellUVMatrix3);
+        computeShader.SetMatrix("octaveUVMatrix4", gridCellUVMatrix4);
+
+        computeShader.SetFloats("cellCounts1", cellCountsArray1);
+        computeShader.SetFloats("cellCounts2", cellCountsArray2);
+        computeShader.SetFloats("cellCounts3", cellCountsArray3);
+        computeShader.SetFloats("cellCounts3", cellCountsArray3);
+        computeShader.SetFloats("cellCounts4", cellCountsArray4);
+
+        computeShader.SetFloat("octaveContribution1", octaveContribution1);
+        computeShader.SetFloat("octaveContribution2", octaveContribution2);
+        computeShader.SetFloat("octaveContribution3", octaveContribution3);
+        computeShader.SetFloat("octaveContribution3", octaveContribution3);
+        computeShader.SetFloat("octaveContribution4", octaveContribution4);
 
     }
     public override void UpdatePreview(){
@@ -65,5 +92,6 @@ public class PerlinNoiseWithOctaves : ShaderManager
         gridCellUVMatrix1 = gridCellUVMatrix1.UpdateTranslationScale2DInt( gridCellNoiseOrigin1, gridCellCount1 );
         gridCellUVMatrix2 = gridCellUVMatrix2.UpdateTranslationScale2DInt( gridCellNoiseOrigin2, gridCellCount2 );
         gridCellUVMatrix3 = gridCellUVMatrix3.UpdateTranslationScale2DInt( gridCellNoiseOrigin3, gridCellCount3 );
+        gridCellUVMatrix4 = gridCellUVMatrix4.UpdateTranslationScale2DInt( gridCellNoiseOrigin4, gridCellCount4 );
     }
 }
--- docs/readme.md
+++ docs/readme.md
@@ -23,7 +23,7 @@
         * and [cooresponding paper](https://mrl.cs.nyu.edu/~perlin/paper445.pdf)
         * in the improved version they start using the cell uv coordinates to generate randomness rather than doing their random vector shenanigans
     - mostly `C`
-* very nice explaination for unity people [](https://jdhwilkins.com/mountains-cliffs-and-caves-a-comprehensive-guide-to-using-perlin-noise-for-procedural-generation/)
+* very nice explaination for unity people [jdhwilkins.com](https://jdhwilkins.com/mountains-cliffs-and-caves-a-comprehensive-guide-to-using-perlin-noise-for-procedural-generation/)
 * how am i just now finding [this glorious deep dive](https://blog.pkh.me/p/42-sharing-everything-i-could-understand-about-gradient-noise.html)
     - they do get a lil mathy with their equations further down but their early breakdowns are really nice
     - it does feel like they skip around slightly for some of the visualisations
